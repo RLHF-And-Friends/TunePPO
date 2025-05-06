bccdc6aa2ac6f2825fee2bfedb3cb089197040f7
Evgurov <evgurovv@yandex.ru>
2025-05-06 04:39:01 +0300

    full datasets loading if None is passed as 'configurations' added

--- ppotune/data/sets/base.py
+++ ppotune/data/sets/base.py
@@ -4,7 +4,11 @@ import torch.distributed as dist
 
 from torch.utils.data import Dataset
 
-from datasets import load_dataset, concatenate_datasets
+from datasets import (
+    load_dataset,
+    concatenate_datasets,
+    get_dataset_config_names
+)
 
 
 class BaseDataset(Dataset):
@@ -21,29 +25,29 @@ class BaseDataset(Dataset):
                 **load_dataset_kwargs
             )
         elif isinstance(configurations, list):
-            self.data = concatenate_datasets([
-                load_dataset(
-                    path=source,
-                    name=configuration,
-                    **load_dataset_kwargs
-                ) for configuration in configurations
-            ])
+            self._load_concat_dataset(source, configurations, **load_dataset_kwargs)
         elif isinstance(configurations, dict):
             worker_configurations = configurations[dist.get_rank()]
-            self.data = concatenate_datasets([
-                load_dataset(
-                    path=source,
-                    name=configuration,
-                    **load_dataset_kwargs
-                ) for configuration in worker_configurations
-            ])
+            self._load_concat_dataset(source, worker_configurations, **load_dataset_kwargs)
         elif configurations is None:
-            self.data = load_dataset(
-                path=source,
-                **load_dataset_kwargs
-            )
+            all_configurations = get_dataset_config_names(source)
+            self._load_concat_dataset(source, all_configurations, **load_dataset_kwargs)
         else:
             raise Exception("Wrong configurations format!")
+
+    def _load_concat_dataset(
+        self,
+        source: str,
+        configurations: list[str],
+        **load_dataset_kwargs
+    ) -> None:
+        self.data = concatenate_datasets([
+            load_dataset(
+                path=source,
+                name=configuration,
+                **load_dataset_kwargs
+            ) for configuration in configurations
+        ])
 
     def __len__(self):
         return len(self.data)
